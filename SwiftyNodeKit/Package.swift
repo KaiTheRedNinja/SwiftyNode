e9b48bb578f4f9966b8fdbe4dce0c06dc47cecf3
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2024-09-07 17:13:02 +0800

    Move macros into SwiftyNodeKit

--- SwiftyNodeKit/Package.swift
+++ SwiftyNodeKit/Package.swift
@@ -2,6 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "SwiftyNodeKit",
@@ -17,7 +18,14 @@ let package = Package(
         .library(
             name: "SwiftyNodeKit",
             targets: ["SwiftyNodeKit"]
-        )
+        ),
+        .library(
+            name: "NodeMacro",
+            targets: ["NodeMacro"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -34,6 +42,21 @@ let package = Package(
             name: "SwiftyNodeKit",
             dependencies: ["Log", "Socket"],
             swiftSettings: [.swiftLanguageMode(.v5)]
-        )
+        ),
+        .macro(
+            name: "NodeMacroMacros",
+            dependencies: [
+                "Log",
+                "SwiftyNodeKit",
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ],
+            path: "Sources/Macro/NodeMacroMacros"
+        ),
+        .target(
+            name: "NodeMacro",
+            dependencies: ["NodeMacroMacros"],
+            path: "Sources/Macro/NodeMacro"
+        ),
     ]
 )
